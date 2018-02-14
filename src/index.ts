3539b4ad8080cd12e2196028793f1fa8237d7933
BrazenIntellect <prashantsinha@live.co.uk>
2018-02-14 19:40:49 +0530

    change of message

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import {Request, Response, Express} from 'express';
 const app: Express = express();
 
 app.get('/', (request: Request, response: Response) => {
-  response.send({hi: 'there'});
+  response.send({hi: 'there from Prashant',
+                profile: 'https://mixcloud.com/thebrazenintellect'});
 });
 
 const PORT = process.env.PORT || 5000;
